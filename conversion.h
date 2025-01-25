2eb0db0c06262e82db36d9b22b1c87e977fe8666
maxxfuu <maxfu826@gmail.com>
2025-01-24 23:11:38 -0800

    added header file

---
+++ conversion.h
@@ -0,0 +1,14 @@
+#ifndef CONVERSION_H
+#define CONVERSION_H
+/* Function Prototype Here */ 
+
+char *hexToDec(const char *input); 
+char *hexToBin(const char *input); 
+
+char *DecToHex(const char *input); 
+char *DecToBin(const char *input); 
+
+char *BinToHex(const char *input); 
+char *BinToDec(const char *input); 
+
+#endif 
